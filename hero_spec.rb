b33e95ed0abf9f738e94cba9ac7fa8dc601f2c5f
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-30 22:10:32 +0300

    Add power_down and hero_info tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -14,5 +14,16 @@ describe Hero do
     expect(hero.power_up).to eq 110
   end
 
+  it "can power down" do
+    hero = Hero.new 'mike'
+
+    expect(hero.power_down).to eq 90
+  end
+
+  it "displays full hero info" do
+    hero = Hero.new 'mike'
+
+    expect(hero.hero_info).to eq 'Mike has a health of 100'
+  end
 
 end

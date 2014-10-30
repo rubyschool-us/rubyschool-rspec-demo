345ef5f1a5ba37ddb7aad92c4fee3ed6966335a5
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-30 22:56:05 +0300

    Add Car class

---
+++ car.rb
@@ -0,0 +1,26 @@
+class Car
+
+  MILES_PER_GALLON = 20
+
+  attr_reader :fuel
+
+  def initialize
+    @fuel = 0
+  end
+
+  def add_fuel amount
+    @fuel += amount
+  end
+
+  # Как далеко мы сможем проехать
+
+  def range
+    0
+  end
+
+end
+
+car = Car.new
+car.add_fuel 10
+puts "Range is #{car.range}" # мы сможем проехать ... миль
+
